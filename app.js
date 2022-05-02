bcf521b31bfcfbddf1428d03aece1bc93c89f88f
derawi <deraw@protonmail.com>
2022-05-02 10:03:21 +0200

    restored Main.jack

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import { Tokenizer } from "./src/Tokenizer.js";
+import { writeFile } from "fs/promises";
 
-(async function main() {
+(async function () {
   console.log(process.argv[2]);
   const tokenizer = new Tokenizer();
   const filepath = "./ArrayTest/Main.jack";
   await tokenizer.init(filepath);
-  const tokenArray = [];
+  const tokenArray = ["<tokens>"];
   while (tokenizer.hasMoreToken()) {
     const currentToken = tokenizer.advance();
     const tokenType = tokenizer.tokenType();
     tokenArray.push(`<${tokenType}> ${currentToken} </${tokenType}>`);
   }
-  console.log(tokenArray);
+  tokenArray.push("</tokens>");
+  await writeFile(`${filepath.slice(0, -5)}T2.xml`, tokenArray.join("\n"));
 })();
